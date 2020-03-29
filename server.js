1693891a8cdf06669ed72a9079e34e674a3fa8cf
kalleja <kalle.jansson@metropolia.fi>
2020-03-29 18:30:13 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,14 +4,29 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const db = require('./database/db');
-const stationRoute = require('./routes/StationRoute');
+const stationRoute = require('./routes/stationRoute');
+const connectionRoute = require('./routes/connectionRoute');
 const port = 3025; 
 
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({extended: true}));
 
+require('./models/connection');
+require('./models/connectionType');
+require('./models/level');
+require('./models/Station');
+require('./models/currentType');
+
+
+
 app.use('/station', stationRoute);
+app.use('/connection', connectionRoute);
 
 db.on('connected', () => { 
   app.listen(port, () => console.log(`App can be run from port ${port}!`));
 });  
+
+
+
+    
+  
