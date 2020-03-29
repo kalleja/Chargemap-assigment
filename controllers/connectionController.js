ab71573746fbd8f713980a89592ce2a735869d5f
kalleja <kalle.jansson@metropolia.fi>
2020-03-29 18:42:18 +0300

    Add files via upload

--- controllers/connectionController.js
+++ controllers/connectionController.js
@@ -12,7 +12,25 @@ const connection_list_get = async (req, res) => {
   }
 };
 
+const connection_get = async (req, res) => {
+  try {
+    const connections = await connectionModel.findById(req.params.id);
+    res.json(connections);
+  } catch (e) {
+    res.send(500).json({
+      message: e.message
+    });
+  }
+};
+
+const connection_post = (req, res) => {
+  res.send('In this forsaken endpoint you can add connection');
+};
+
 module.exports = {
   connection_list_get,
+  connection_get,
+  connection_post,
+
 };
 
