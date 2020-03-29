75adef72c78db62cc1e941c84881d021cf3f7e61
kalleja <kalle.jansson@metropolia.fi>
2020-03-29 12:39:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const express = require('express');
 const app = express();
 const db = require('./database/db');
 const stationRoute = require('./routes/StationRoute');
-const port = 3000; 
+const port = 3025; 
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/station', stationRoute);
 
 db.on('connected', () => { 
-  app.listen(port, () => console.log(`app listening on port ${port}!`));
-});
+  app.listen(port, () => console.log(`App can be run from port ${port}!`));
+});  
