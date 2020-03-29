9f622585c8381fd8545134937348d99f2d4bcd2d
kalleja <kalle.jansson@metropolia.fi>
2020-03-29 18:41:29 +0300

    Add files via upload

--- routes/connectionRoute.js
+++ routes/connectionRoute.js
@@ -4,6 +4,10 @@ const express = require('express');
 const router = express.Router();
 const connectionController = require('../controllers/connectionController');
 
-router.get('/', connectionController.connection_list_get);
+router.get('/', connectionController.connection_list_get); 
+
+router.get('/:id', connectionController.connection_get);
+
+router.post('/', connectionController.connection_post);
 
 module.exports = router;
